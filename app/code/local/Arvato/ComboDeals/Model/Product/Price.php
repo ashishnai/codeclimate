1521e06351020b0610044b05b49465b0663f057e
shireen <shireenn@cybage.com>
2015-11-27 12:02:16 +0100

    Deleted unnecessary files and corrected the file merge issue dyring file transfer

--- app/code/local/Arvato/ComboDeals/Model/Product/Price.php
+++ app/code/local/Arvato/ComboDeals/Model/Product/Price.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * Adminhtml product edit tabs
- *
- * @category    Arvato
- * @package     Arvato_ComboDeals
- * @author      Shireen N <shireenn@cybage.com>
- */
-
-class Arvato_ComboDeals_Model_Product_Type extends Mage_Bundle_Model_Product_Type
-
+class Arvato_ComboDeals_Model_Product_Price extends Mage_Catalog_Model_Product_Type_Price
 {
-	const TYPE_COMBODEAL = 'combodeal';
+
 }
