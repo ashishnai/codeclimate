36c357a6996c34e81d9ecfa6e96d0514e69d1da4
shireen <shireenn@cybage.com>
2015-12-08 15:28:18 +0100

    Code review changes

--- app/code/local/Arvato/ComboDeals/Helper/Data.php
+++ app/code/local/Arvato/ComboDeals/Helper/Data.php
@@ -7,9 +7,6 @@
  * @copyright   Copyright (c) arvato 2015
  */
 
-/**
- * Arvato data helper
- */
 class Arvato_ComboDeals_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const VALIDATION_CLASS_HAS_PRODUCTS = 'validate-has-products';
