d216af5f02507e43f22edc39da75216733269749
mayurpate <mayurpate@cybage.com>
2015-12-23 19:06:22 +0530

    Added frontend template file and admin block file and changed modman file for 'arvato_basis'

---
+++ app/code/local/Arvato/ComboDeals/Block/Category/Alldeals.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * All combodeals dedicated page
+ * 
+ * @category    Arvato
+ * @package     Arvato_ComboDeals
+ * @copyright   Copyright (c) arvato 2015
+ */
+class Arvato_ComboDeals_Block_Category_Alldeals extends Mage_Catalog_Block_Category_View
+{
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+    }
+}
